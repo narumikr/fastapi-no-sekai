2e7190cf6631e975cea6d546d19943ee307d100f
Narumikr <narunarumikumiku396@gmail.com>
2026-06-05 01:32:44 +0900

    ドメインエラーの基底クラス作成

---
+++ app/contexts/shared/exceptions.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class ErrorDetails:
+    """エラーの詳細情報を表すクラス
+
+    エラーの詳細情報はフィールド名とメッセージを含むことができる
+    """
+
+    field: str | None = None  # エラーが発生したフィールド名
+    message: str  # エラーの説明
+
+
+class BussinessException(Exception):
+    """ドメイン層の例外を表す基底クラス
+
+    ドメイン層の例外はすべてこのクラスを継承して定義する
+    """
+
+    def __init__(
+        self, code: str, message: str, details: list[ErrorDetails] | None = None
+    ):
+        self.code = code  # エラーの識別子
+        self.message = message  # エラーの説明
+        self.details = details if details is not None else []  # エラーの詳細情報
+        super().__init__(message)
