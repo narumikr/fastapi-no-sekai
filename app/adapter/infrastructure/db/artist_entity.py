99e7ea5b32647a640753da3fe134740ac440eccd
Narumikr <narunarumikumiku396@gmail.com>
2026-06-09 22:34:09 +0900

    依存関係のおかしかったファイルの配置場所を修正

--- app/adapter/infrastructure/db/artist_entity.py
+++ app/adapter/infrastructure/db/artist_entity.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
 
-from app.core.database import BaseEntity
+from app.adapter.infrastructure.db.base_entity import BaseEntity
 
 
 class ArtistEntity(BaseEntity):
