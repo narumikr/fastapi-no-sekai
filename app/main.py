ea1c5d518866a6ed582ab169c77165e5fb77117c
Narumikr <narunarumikumiku396@gmail.com>
2026-06-15 22:52:59 +0900

    fix review

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ import os
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
-from sqlalchemy import engine
 
 from app.adapter.controllers.artist_controller import router as artist_router
 from app.adapter.infrastructure.db.base_entity import Base
@@ -10,7 +9,7 @@ from app.core.database import engine
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
-    if os.getenv("DATABASE_SYNC") == "true":
+    if os.getenv("DATABASE_SYNC", "false").lower() == "true":
         import app.adapter.infrastructure.db  # noqa: F401
 
         Base.metadata.create_all(bind=engine)
